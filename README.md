d68f83265b8da402a0ff25adedf83615974a2661
Umesh Khadka <quoriumesh@gmail.com>
2024-01-31 10:09:35 +0545

    Added Transaction

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ifunds
+# ifunds <br>
+This is the project repository for the e-wallet project iFunds.
--- ifunds/main.cpp
+++ ifunds/main.cpp
@@ -2,15 +2,91 @@
 #include"user.h"
 #include"DeleteAccount.h"
 #include"Transaction.h"
+#include "Registration.h"
 
 int main() {
 
-	//DeleteAccount delete_account;
-	//delete_account.confirmDeletion();
 
-	Transaction make_transaction;
+    //goto dash;
+    // after dashboard
+//faceauthentication:
+    // after face authentication: 
+    system("cls");
 
-	make_transaction.initiateTransaction();
 
+     mainmenu:
+        cout << "------------------------------------------------------------------------------------------------\n\n";
+        cout << "                                      WELCOME TO MAIN MENU!!!\n\n";
+        cout << "------------------------------------------------------------------------------------------------\n\n\n";
+        cout << " enter 1 to add funds\n";
+        cout << " enter 2 for transaction history\n";
+        cout << " enter 3 to initiate transaction\n";
+        cout << " enter 4 for account management\n";
+        cout << " enter 5 for currency convertern\n";
+        cout << " enter 6 for deleting your account\n";
+        cout << " enter 7 to log out\n";
+        cout << " enter 8 to exit \n";
+        cout << "------------------------------------------------------------------------------------------------\n\n\n";
+
+        int b;
+        cout << "enter your choice:\t";
+        cin >> b;
+        cout << "*********************************************************************************************************************\n\n";
+
+        system("cls");
+        switch (b)
+        {
+        /*case 1:
+
+
+            cout << "------------------------------------------------------------------------------------------------\n\n";
+            cout << "                                 WELCOME TO ADD FUNDS\n\n";
+            cout << "------------------------------------------------------------------------------------------------\n\n";
+            //goto ADD;
+        //case 2:
+        */
+        case 3: {
+            Transaction make_transaction;
+
+            make_transaction.initiateTransaction();
+        }
+        case 6: {
+            DeleteAccount delete_account;
+            delete_account.confirmDeletion();
+        }
+            
+        /*case 7:
+            char ch;
+            cout << "------------------------------------------------------------------------------------------------\n\n";
+            cout << "                                 LOGOUT\n\n";
+            cout << "------------------------------------------------------------------------------------------------\n\n";
+            cout << "are yoou sure to logout??\t press 'y' for yes \n";
+            cin >> ch;
+            if (ch == 'y' && 'y')
+            {
+                goto dash;
+            }
+            else
+            {
+                goto mainmenu;
+            
+
+        case 8:
+
+            cout << "sure to exit:\t press 'y' for yes \n";
+            cin >> ch;
+            if (ch == 'y' && 'y')
+            {
+                system("cls");
+                exit(0);
+            }
+            else
+            {
+                goto mainmenu;
+            }
+            //ADD:*/
+
+        }
+	
 	return 0;
 }
